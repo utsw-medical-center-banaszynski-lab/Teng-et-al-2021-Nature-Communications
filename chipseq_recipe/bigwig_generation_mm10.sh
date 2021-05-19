6e72a08dbc2f4271775340219b633cc17be59a83
ASundaresan <44269211+asundaresan1@users.noreply.github.com>
2021-05-19 16:55:58 -0700

    Update bigwig_generation_mm10.sh

--- chipseq_recipe/bigwig_generation_mm10.sh
+++ chipseq_recipe/bigwig_generation_mm10.sh
@@ -1,15 +1,5 @@
 #!/bin/tcsh
 
-#SBATCH --job-name=make-bw                            # job name
-#SBATCH --partition=super                                # select partion from 128GB, 256GB, 384GB, GPU and super
-#SBATCH --nodes=1                                        # number of nodes requested by user
-#SBATCH --time=0-72:00:00                                 # run time, format: D-H:M:S (max wallclock time)
-#SBATCH --output=serialJob.%j.out                        # standard output file name
-#SBATCH --error=serialJob.%j.time                        # standard error output file name
-#SBATCH --mail-user=aishwarya.sundaresan@utsouthwestern.edu      # specify an email address
-#SBATCH --mail-type=ALL                                  # send email when job status change (start, end, abortion and etc.)
-
-
 module load samtools
 module load picard/1.117
 module load bedtools
@@ -17,27 +7,19 @@ module load UCSC_userApps
 module load macs/1.4.2
 module load igvtools/2.3.71
 
-#mkdir bam_files
-
 foreach sample(\
-   XP164_K36me3_input_S9\
-   XP250_K36me3_input_S10\
-   XP369_K36me3_input_S11\
-   XP490_K36me3_input_S8\
-   XPL183_K36me3_input_S12\
+   Sample1\
+   Sample2\
 )  
 
 echo $sample
 
 
    set CHROM_SIZE = /project/apps_database/iGenomes/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa.fai
-    #set CHROM_SIZE = /project/apps_database/iGenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa.fai
+    
+   bedtools genomecov -ibam $sample.filtered.no.downsampled.bam -bga > $sample.nodup.bedGraph
 
-    #mkdir ./bw_files/read_depth_nf/mm10/
+    sort -k1,1 -k2,2n $sample.nodup.bedGraph > $sample.nodup.sorted.bedGraph
 
-    bedtools genomecov -ibam /project/GCRB/Banaszynski_lab/shared/Haitao/ChIP-Seq/run38_2018_12_04/AS_analysis/NChIP_pipeline/downsample_out/read_depth_nf/mm10/downsample.sh-1.1.0/$sample.filtered.no.downsampled.bam -bga > ./bw_files/read_depth_nf/mm10/$sample.nodup.bedGraph
-
-    sort -k1,1 -k2,2n ./bw_files/read_depth_nf/mm10/$sample.nodup.bedGraph > ./bw_files/read_depth_nf/mm10/$sample.nodup.sorted.bedGraph
-
-    bedGraphToBigWig ./bw_files/read_depth_nf/mm10/$sample.nodup.sorted.bedGraph $CHROM_SIZE ./bw_files/read_depth_nf/mm10/$sample.nodup.bw
+    bedGraphToBigWig $sample.nodup.sorted.bedGraph $CHROM_SIZE $sample.nodup.bw
 end
